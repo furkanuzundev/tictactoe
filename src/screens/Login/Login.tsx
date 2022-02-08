6bb6d8bf5636ed67e298bd26bfab8b8b76872847
Furkan <furkanu48@gmail.com>
2022-02-08 07:37:02 +0300

    feat:design login page

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -1,18 +1,94 @@
-import * as React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import React from 'react';
+import {useState} from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
 
-interface LoginProps {}
+import colors from '../../constants/colors';
+import images from '../../constants/images';
+import theme from '../../constants/theme';
+import fonts from '../../constants/fonts';
 
-const Login = (props: LoginProps) => {
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
+
+const Login = () => {
+  const [userName, setUsername] = useState<string>('');
+
   return (
-    <View style={styles.container}>
-      <Text>Login</Text>
-    </View>
+    <KeyboardAwareScrollView contentContainerStyle={styles.keyboard}>
+      <View style={styles.container}>
+        <Image source={images.logo} style={styles.logo} resizeMode="contain" />
+        <View>
+          <Text style={styles.description}>
+            Enter your name, let the fun begin!
+          </Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Name"
+            onChangeText={name => setUsername(name)}
+            value={userName}
+            selectionColor={colors.primary}
+          />
+        </View>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>GO!</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAwareScrollView>
   );
 };
 
 export default Login;
 
 const styles = StyleSheet.create({
-  container: {},
+  keyboard: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  logo: {
+    width: 200,
+    height: 200,
+  },
+  description: {
+    fontFamily: fonts.indie,
+    fontSize: 18,
+    color: colors.black,
+  },
+  button: {
+    width: 100,
+    padding: 10,
+    backgroundColor: colors.primary,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    ...theme.shadow,
+  },
+  buttonText: {
+    fontFamily: fonts.indie,
+    fontSize: 25,
+    color: colors.yellow,
+  },
+  input: {
+    backgroundColor: colors.white,
+    marginTop: 10,
+    borderRadius: 10,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontFamily: fonts.indie,
+    textAlign: 'center',
+    fontSize: 25,
+    color: colors.black,
+    ...theme.shadow,
+  },
 });
