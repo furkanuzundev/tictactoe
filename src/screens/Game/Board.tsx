54399d9578cdf495e9c1a3f3d82115938eb9fea6
Furkan <furkanu48@gmail.com>
2022-02-10 11:53:36 +0300

    feat: check winner end of the game

--- src/screens/Game/Board.tsx
+++ src/screens/Game/Board.tsx
@@ -1,8 +1,10 @@
+import {CommonActions, useNavigation} from '@react-navigation/native';
 import React from 'react';
 import {useEffect} from 'react';
 import {useState} from 'react';
-import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
-import {useSelector} from 'react-redux';
+import {Text, View, StyleSheet, TouchableOpacity, Alert} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
+import board from '../../constants/board';
 
 import colors from '../../constants/colors';
 import {SCREEN_WIDTH} from '../../constants/screen';
@@ -10,12 +12,18 @@ import theme from '../../constants/theme';
 import {RootState} from '../../store';
 
 import {checkWinner} from '../../utils/winnerHandler';
+import ResultModal from './ResultModal';
+import {listenCurrentGame} from '../../store/game/actions';
 
 interface BoardProps {}
 
 const Board = (props: BoardProps) => {
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
+
   const {game, user} = useSelector((store: RootState) => store);
   const [allowTouch, setAllowTouch] = useState<boolean>(true);
+  const [modal, setModal] = useState<any>(null);
 
   let myTurn = game.currentGame.data().turn.uid === user.currentUser.uid;
 
@@ -30,14 +38,13 @@ const Board = (props: BoardProps) => {
   }, [game.currentGame, myTurn]);
 
   const renderBoard = () => {
-    const board = game.currentGame.data().board;
-    const length = Object.keys(board).length;
+    const currentBoard = game.currentGame.data().board;
+    const length = Object.keys(currentBoard).length;
 
-    return Object.values(board).map((rowItem, rowIndex) => {
+    return Object.values(currentBoard).map((rowItem, rowIndex) => {
       return (
         <View style={styles.row} key={rowIndex}>
           {renderColumns(rowItem, rowIndex, length)}
-          <View style={styles.line} />
         </View>
       );
     });
@@ -67,6 +74,21 @@ const Board = (props: BoardProps) => {
     });
   };
 
+  useEffect(() => {
+    const currentGame = game.currentGame.data();
+    const boardLength = Math.pow(Object.keys(currentGame.board).length, 2);
+
+    if (currentGame.steps === 0) {
+      return;
+    }
+
+    if (currentGame.winner) {
+      showWinner(currentGame);
+    } else if (boardLength === currentGame.steps) {
+      showDraw();
+    }
+  }, [game.currentGame]);
+
   const move = (rowIndex: number, columnIndex: number) => {
     const updatedGame = {...game.currentGame.data()};
 
@@ -79,6 +101,7 @@ const Board = (props: BoardProps) => {
     );
 
     updatedGame.board[rowIndex][columnIndex] = me.mark;
+    updatedGame.steps = updatedGame.steps + 1;
 
     let winner = checkWinner(
       updatedGame.board,
@@ -86,25 +109,93 @@ const Board = (props: BoardProps) => {
       me.mark,
     );
 
-    console.log('winner : ', winner);
+    const boardLength = Math.pow(Object.keys(updatedGame.board).length, 2);
 
-    if (myTurn) {
-      updatedGame.turn = away;
+    if (winner) {
+      updatedGame.winner = {
+        moves: winner,
+        user: myTurn ? me : away,
+      };
+      updatedGame.status = 3;
+    } else if (boardLength === updatedGame.steps) {
+      const resetBoard = board.grids.find(
+        item => item.dimension === updatedGame.dimension,
+      );
+
+      updatedGame.board = resetBoard.data;
+      updatedGame.turn = updatedGame.between[0];
+      console.log('updatedGame DRAW :', updatedGame);
     } else {
-      updatedGame.turn = me;
+      updatedGame.turn = myTurn ? away : me;
     }
 
     game.currentGame.ref.update(updatedGame);
   };
 
+  const resetNavigation = () => {
+    navigation.dispatch(
+      CommonActions.reset({
+        index: 1,
+        routes: [{name: 'Main'}],
+      }),
+    );
+  };
+
+  const showWinner = (currentGame: any) => {
+    setModal(
+      <ResultModal
+        title={
+          currentGame.winner.user.uid === user.currentUser.uid
+            ? 'YOU WIN!'
+            : 'YOU LOSE!'
+        }
+        description="You are being redirected to the homepage."
+        onOpened={onShowOpened}
+      />,
+    );
+  };
+
+  const onShowOpened = () => {
+    const timer = setTimeout(() => {
+      setModal(null);
+      dispatch(listenCurrentGame(null));
+      resetNavigation();
+    }, 1500);
+    return () => clearTimeout(timer);
+  };
+
+  const showDraw = () => {
+    setModal(
+      <ResultModal
+        title="DRAW!"
+        description="Game restarting."
+        onOpened={onDrawOpened}
+      />,
+    );
+
+    game.currentGame.ref.update({...game.currentGame, steps: 0});
+  };
+
+  const onDrawOpened = () => {
+    const timer = setTimeout(() => {
+      setTimeout(() => {
+        setModal(null);
+      }, 1500);
+    }, 1500);
+    return () => clearTimeout(timer);
+  };
+
   return (
     <View style={styles.container} pointerEvents={allowTouch ? 'auto' : 'none'}>
+      {modal}
       {renderBoard()}
     </View>
   );
 };
 
 export default Board;
+
+// pointerEvents={allowTouch ? 'auto' : 'none'}
 
 const styles = StyleSheet.create({
   container: {
@@ -126,5 +217,14 @@ const styles = StyleSheet.create({
   },
   line: {
     position: 'absolute',
+    width: 100,
+    height: 5,
+    backgroundColor: colors.white,
+    borderRadius: 5,
+    transform: [
+      {
+        translateX: 10,
+      },
+    ],
   },
 });
